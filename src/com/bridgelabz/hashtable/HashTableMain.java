ec25b793356830c2f37d421a9806d2e776e5def8
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-01 20:58:02 +0530

    ADDED: add and get methods added to MyLinkedHashMap

--- src/com/bridgelabz/hashtable/HashTableMain.java
+++ src/com/bridgelabz/hashtable/HashTableMain.java
@@ -3,19 +3,10 @@ package com.bridgelabz.hashtable;
 public class HashTableMain {
 	
 	public static void main(String[] args) {
-		String sentence="To be or not to be";
-		MyHashMap<String,Integer> myHashMap=new MyHashMap<>();
-		String[] words=sentence.toLowerCase().split(" ");
-		for(String word : words) {
-			Integer value=myHashMap.get(word);
-			if(value == null) 
-				value=1;
-			else
-				value=value+1;
-			myHashMap.add(word,value);
-		}
-		System.out.println("Frequency of the word to is"+myHashMap.get("to"));
-		System.out.println(myHashMap);
+		String sentence="Paranoids are not paranoid because they are paranoid but"+
+				"because they keep putting themselves"+
+				"deliberately into paranoid avoidable situations" ;
+		
 	}
 
 }
--- src/com/bridgelabz/hashtable/MyLinkedHashMap.java
+++ src/com/bridgelabz/hashtable/MyLinkedHashMap.java
@@ -12,4 +12,36 @@ public class MyLinkedHashMap<K,V> {
 		for(int i=0;i<numBuckets;i++)
 			this.myBuckeyArray.add(null);
 	}
+	
+	private int getBucketIndex(K key) {
+		int hashCode=Math.abs(key.hashCode());
+		int index=hashCode% numBuckets;
+		System.out.println("Key: "+key+" hashcode: "+hashCode+" index: "+index);
+		return index;
+	}
+	
+	public V get(K key) {
+		int index=this.getBucketIndex(key);
+		LinkedList<K> myLinkedList=this.myBuckeyArray.get(index);
+		if(myLinkedList==null)
+			return null;
+		MyMapNode<K,V> myMapNode=(MyMapNode<K,V>) myLinkedList.search(key);
+		return (myMapNode==null)?null:myMapNode.getValue();
+	}
+
+	public void add(K key, V value) {
+		int index=this.getBucketIndex(key);
+		LinkedList<K> myLinkedList=this.myBuckeyArray.get(index);
+		if(myLinkedList==null) {
+			myLinkedList=new LinkedList<>();
+			this.myBuckeyArray.set(index, myLinkedList);
+		}
+		MyMapNode<K,V> myMapNode=(MyMapNode<K,V>)myLinkedList.search(key);
+		if(myMapNode==null) {
+			myMapNode=new MyMapNode<K, V>(key, value);
+			myLinkedList.append(myMapNode);
+		}else {
+			myMapNode.setValue(value);
+		}
+	}
 }
